0fe9f7cf1d8945cb48d6a4c5f4403d967ef76288
sadanan vorlaruxtara <sadanan.vorlaruxtara@gmail.com>
2024-08-07 16:52:32 +0900

    Completed restaurant review addition

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,4 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
-  get 'reviews/create'
-  get 'restaurants/index'
-  get 'restaurants/show'
-  get 'restaurants/new'
-  get 'restaurants/create'
-  get 'restaurants/edit'
-  get 'restaurants/update'
-  get 'restaurants/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -16,8 +7,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
- resources :restaurants, only: [:index, :show ,:new, :create] do
-  resources :reviews, only: [:new, :create]
- end
- resources :reviews, only: [:edit, :update, :destroy]
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
+
+  resources :reviews, only: [:edit, :update, :destroy]
 end
